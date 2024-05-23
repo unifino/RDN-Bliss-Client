ecd0f27ea69adddbdf15f19873bef29e94d22e42
unifino <unifino@protonmail.com>
2024-05-23 22:51:03 +0330

    Hello

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <BG msg="Nutirition"/>
+    <Home />
+</template>
+
+// -- =====================================================================================
+
+<script lang="ts">
+
+import { Options, Vue }             from 'vue-class-component';
+import BG                           from './components/BG.vue';
+import Home                         from './components/Home.vue';
+
+// -- =====================================================================================
+
+@Options( {
+    components: {
+        BG, Home,
+    },
+} )
+export default class App extends Vue {}
+
+// -- =====================================================================================
+
+</script>
+
+// -- =====================================================================================
+
+<style scoped>
+
+#app {
+    background-color: red;
+    padding: 0px;
+    margin: 0px;
+}
+
+</style>
+
+// -- =====================================================================================
