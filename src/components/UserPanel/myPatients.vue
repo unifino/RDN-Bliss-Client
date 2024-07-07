c4ef011e96a22988bf8cbdbb7e789283ac8e867c
unifino <unifino@protonmail.com>
2024-07-07 20:07:28 +0330

    ...

--- src/components/UserPanel/myPatients.vue
+++ src/components/UserPanel/myPatients.vue
@@ -1,6 +1,13 @@
-        <template>
-    <div id="my_patients_box" class="init" ref="message">
-        
+<template>
+    <div id="my_patients_box" class="init" ref="patientsBox">
+        <div id="myWrapper">
+            <div :class="'patientBox ' + p.sex" v-for="p in patients" :key="p.id">
+                <div class="nameWrapper">
+                    <div class="name no_select">{{ p.name }}</div>
+                </div>
+            </div>
+        </div>
+        <div id="borderTop" />
     </div>
 </template>
 
@@ -17,7 +24,7 @@ const store: TS.Store = useStore()
 
 // -- =====================================================================================
 
-    const message = ref<HTMLElement>( {} as HTMLElement )
+    const patientsBox = ref<HTMLElement>( {} as HTMLElement )
 
 // -- =====================================================================================
 
@@ -30,9 +37,26 @@ const store: TS.Store = useStore()
         if ( phase === "In" ) 
             if ( !skip ) 
                 await new Promise( _ => setTimeout( _, Tools.speed() *.8 ) )
-        message.value.className = "Tool_fall_" + phase
+        patientsBox.value.className = "Tool_fall_" + phase
     }
 
+// -- =====================================================================================
+
+    const patients = [
+        { id: 0, sex:"male",   name: "Hatef" },
+        { id: 0, sex:"male",   name: "Ali" },
+        { id: 0, sex:"male",   name: "Rasul" },
+        { id: 0, sex:"female", name: "Fatemeh" },
+        { id: 0, sex:"female", name: "Sara" },
+        { id: 0, sex:"male",   name: "Farid" },
+        { id: 0, sex:"male",   name: "Javad" },
+        { id: 0, sex:"male",   name: "Saeed" },
+        { id: 0, sex:"female", name: "Leyla" },
+        { id: 0, sex:"male",   name: "HamidReza" },
+        { id: 0, sex:"male",   name: "Mohaddese" },
+        { id: 0, sex:"male",   name: "Karim" },
+        { id: 0, sex:"female", name: "Sima" },
+    ]
 
 // -- =====================================================================================
 
@@ -65,19 +89,75 @@ const store: TS.Store = useStore()
 <style scoped>
 
     #my_patients_box{
-        background-color: #545454;
+        background-color: #e6e3e3;
         height: 620px;
-        width: 700px;
+        width: 780px;
         top: 50%;
         left: 300px;
-        padding: 40px;
+        border: solid 40px #e6e3e3;
+        border-top-width: 10px;
         border-radius: 23px;
         box-shadow: 0 0 7px 1px #babbbb;
         position: absolute;
     }
+    
+    #myWrapper{
+        height: calc( 100% - 30px );
+        padding-top: 30px;
+        border-bottom: solid 40px #e6e3e3;
+        overflow: auto;
+    }
+
+    #borderTop{
+        background-color: #e6e3e3de;
+        height: 40px;
+        width: 100%;
+        top: 0px;
+        position: absolute;
+    }
+
+    .patientBox{
+        height: 180px;
+        width: 240px;
+        background-color: #d3d3d3;
+        position: relative;
+        float: left;
+        margin: 10px;
+        border-radius: 5px;
+        overflow: hidden;
+        cursor: pointer;
+        background-position: left 15px;
+        background-size: auto 70%;
+        background-repeat: no-repeat;
+    }
+
+    .nameWrapper{
+        text-align: right;
+        background-color: #929292d5;
+        height: 34px;
+        width: 100%;
+        line-height: 34px;
+        bottom: 0;
+        padding: 3px 0px;
+        position: absolute;
+    }
+    .male{ background-image: url('@/assets/Pics/Avatars/ai-generated-1.png') }
+    .female{ background-image: url('@/assets/Pics/Avatars/ai-generated-2.png') }
+    .male> div{ background-color: #67c3dcd5; }
+    .female> div{ background-color: #e77fc1d5 }
+    
+    .name{
+        color: white;
+        font-family: Oswald;
+        font-weight: bold;
+        font-size: 20px;
+        right: 20px;
+        position: absolute;
+    }
 
     .init{
-        transform: translate(-10%,-53%) perspective(900px) rotateY(44deg) scale(.8); opacity: 0
+        transform: translate(-10%,-53%) perspective(900px) rotateY(44deg) scale(.8); opacity: 0;
+        transform: translate(0%,-53%) scale(1); opacity: 1
     }
 
 </style>
