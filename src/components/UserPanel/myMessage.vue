339a670b137261b565f27b57b2157b96f32351c1
unifino <unifino@protonmail.com>
2024-07-08 08:23:07 +0330

    ...

--- src/components/UserPanel/myMessage.vue
+++ src/components/UserPanel/myMessage.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="my_message_box" class="init" ref="message">
-        
+        <div id="messagesBox">
+            <textarea />
+        </div>
+        <div id="buttonsWrapper">
+            <div class="button" v-for="(b,i) of buttons" :key="i">{{ b.title }}</div>
+        </div>
     </div>
 </template>
 
@@ -21,8 +26,16 @@ const store: TS.Store = useStore()
 
 // -- =====================================================================================
 
+    const buttons = [ 
+        { title: "New Messages" },
+        { title: "All Messages" },
+        { title: "Contact List" },
+        { title: "Archive" },
+    ]
+
     const _out = () => myAnimation( "Out" )
     const _in = ( skip = false ) => myAnimation( "In", skip )
+
 
 // -- =====================================================================================
 
@@ -65,21 +78,57 @@ const store: TS.Store = useStore()
 <style scoped>
 
     #my_message_box{
-        background-color: #e9e8e6;
-        height: 620px;
-        width: 700px;
+        background-color: transparent;
+        height: 700px;
+        width: 780px;
         top: 50%;
         left: 300px;
-        padding: 40px;
-        border-radius: 23px;
-        box-shadow: 0 0 7px 1px #babbbb;
         position: absolute;
     }
 
     .init{
-        transform: translate(-10%,-53%) perspective(900px) rotateY(44deg) scale(.8); opacity: 0
+        transform: translate(-10%,-53%) perspective(900px) rotateY(44deg) scale(.8); opacity: 0;
     }
 
+    #messagesBox{
+        background-color: #e9e8e6;
+        height: 100%;
+        width: 450px;
+        box-shadow: 0 0 7px 1px #babbbb;
+        border-radius: 23px;
+        position: relative;
+        float: left;
+    }
+
+    #messagesBox textarea{
+        bottom: 30px;
+        min-height: 50px;
+        min-width: 90%;
+        max-width: 90%;
+        margin: auto;
+        left: 0;
+        right: 0;
+        border: none;
+        border-radius: 9px;
+        position: absolute;
+    }
+    #buttonsWrapper{
+        width: 200px;
+        position: relative;
+        float: left;
+        margin-left: 20px;
+        margin-top: 50px;
+    }
+
+    .button{
+        color: #144444;
+        font-family: Manrope;
+        font-weight: 600;
+        margin: 12px;
+        cursor: pointer;
+    }
+
+    .button:hover{ color: #08cee0; }
 
 </style>
 
